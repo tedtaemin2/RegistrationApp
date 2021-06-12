601de03a8ed350717f6e9e24599ff2d8cf3de2ce
Ted <tedtaemin@gmail.com>
2021-06-11 23:41:26 -0700

    debugging connection to db

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -7,17 +7,17 @@ class RegistrationControllerBlueprint {
       const {
         firstName, lastName, address1, address2, city, state, zipcode, country, timeStamp,
       } = req.body;
-
       // query by descending timestamp
       const queryString = 'INSERT INTO registration (First_Name, Last_Name, Address_1, Address_2, City, State, Zip_Code, Country, Time_Stamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
       const result = await db.query(queryString, [firstName, lastName, address1, address2, city, state, zipcode, country, timeStamp]);
-      console.log(result);
       next();
     } catch (error) {
       console.log(error);
     }
   }
 }
+const registrationController = new RegistrationControllerBlueprint();
+module.exports = registrationController;
 
 /*
 **** Below is the code for server side form validation which led to an error. If I had more time, I would have liked to debug this issue.
