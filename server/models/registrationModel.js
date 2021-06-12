092b504c62c0bae334d6f0f04630544c7617b9ed
Ted <tedtaemin@gmail.com>
2021-06-11 22:25:56 -0700

    debugging connection to db

--- server/models/registrationModel.js
+++ server/models/registrationModel.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-
+  connectionString: 'postgres://ouyyrxwpjlvxri:09f52c04d824af4f5865c98ef4353178a117a729cb2b65cddbba33c7c54a9f96@ec2-23-23-128-222.compute-1.amazonaws.com:5432/df813d0cf5qd7',
   port: 5432,
   user: 'ouyyrxwpjlvxri',
   password: '09f52c04d824af4f5865c98ef4353178a117a729cb2b65cddbba33c7c54a9f96',
