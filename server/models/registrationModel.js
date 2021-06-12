fe5c978a556adb96d010f9671d3e6f6db69c5630
Ted <tedtaemin@gmail.com>
2021-06-11 21:29:51 -0700

    changed pool to client in db

--- server/models/registrationModel.js
+++ server/models/registrationModel.js
@@ -1,14 +1,37 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Client } = require('pg');
 
-const pool = new Pool({
-
-  connectionString: process.env.PG_URI,
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = {
-  query: (text, params, callback) => {
-    console.log('Your query: ', text);
-    return pool.query(text, params, callback);
-  },
-};
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+module.export = client;
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+
+//   connectionString: process.env.PG_URI,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// module.exports = {
+//   query: (text, params, callback) => {
+//     console.log('Your query: ', text);
+//     return pool.query(text, params, callback);
+//   },
+// };
